eccd48ee97111f111cfcc9a94c09472fd59c4ceb
Asranov <asranov0003@gmail.com>
2023-08-03 10:16:15 +0500

    animal

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("js working");
+//Basic Class and Object
+
+class Animal {
+  constructor(name, sound) {
+    this.name = name;
+    this.sound = sound;
+  }
+
+  makeSound() {
+    console.log(`${this.name} says ${this.sound}`);
+  }
+}
+
+const cat = new Animal("Cat", "Meaw");
+cat.makeSound();
+
+// const createAnimal = (name, sound) => {
+//   function makeSound() {
+//     console.log(`${name} says ${sound}`);
+//   }
+
+//   return makeSound;
+// };
+
+// const dog = createAnimal("Dog", "Woof");
+// dog();
