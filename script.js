9442b6e6fc14321fe7b1ba4f451a6426087f0673
Asranov <asranov0003@gmail.com>
2023-08-03 10:48:59 +0500

    advance oop

--- script.js
+++ script.js
@@ -68,3 +68,142 @@ const myAccount = new BankAccount("123456789", 1000);
 console.log(myAccount.getBalance());
 myAccount.deposit(5000);
 console.log(myAccount.getBalance());
+
+// const testBankAccount = (accountNumber, balance) => {
+//   return {
+//     getBalance: function () {
+//       return balance;
+//     },
+//     deposit: function (amount) {
+//       balance += amount;
+//     },
+//   };
+// };
+
+// const testAccount = testBankAccount(123456789, 1200);
+// console.log(testAccount.getBalance());
+// testAccount.deposit(300);
+// console.log(testAccount.getBalance());
+
+class Shape {
+  constructor(color) {
+    this.color = color;
+  }
+}
+
+class Circle extends Shape {
+  constructor(color, radius) {
+    super(color);
+    this.radius = radius;
+  }
+
+  draw() {
+    console.log(
+      `Drawing a circle with color ${this.color} and radius ${this.radius}`
+    );
+  }
+}
+
+const redCircle = new Circle("red", 5);
+
+//Task 1: Create a class representing a Person with properties name and age. Add a method to introduce the person.
+class PersonClass {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`${this.name} is ${this.age} years old`);
+  }
+}
+
+//Task 2: Create a class representing a Car with properties make, model, and year. Add a method to display information about the car.
+
+class CarClass {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  displayInfo() {
+    console.log(`${this.make} ${this.model} was made in ${this.year}`);
+  }
+}
+
+//Task 3: Create a class representing a Book with properties title, author, and publication year. Add a method to display information about the book.
+
+class BookClass {
+  constructor(title, author, publicationYear) {
+    this.title = title;
+    this.author = author;
+    this.publicationYear = publicationYear;
+  }
+
+  displayInfo() {
+    console.log(
+      `${this.title} by ${this.author} was published in ${this.publicationYear}`
+    );
+  }
+}
+
+//Task 4: Create a class representing a Student with properties name and grades (an array of numbers). Add a method to calculate the average grade of the student.
+
+class StudentClass {
+  constructor(name, grades) {
+    this.name = name;
+    this.grades = grades;
+  }
+
+  averageGrade() {
+    let sum = 0;
+    for (let i = 0; i < this.grades.length; i++) {
+      sum += this.grades[i];
+    }
+    return sum / this.grades.length;
+  }
+}
+
+//Task 5: Create a class representing a Teacher with properties name and subject. Add a method to display the teacher's subject.
+
+class TeacherClass {
+  constructor(name, subject) {
+    this.name = name;
+    this.subject = subject;
+  }
+
+  displaySubject() {
+    console.log(this.subject);
+  }
+}
+
+//Task 6: Create a class representing a Bank with properties name and accounts (an array of BankAccount objects). Add methods to open a new account, close an account, and calculate the total balance of all accounts.
+
+class BankAccountClass {
+  constructor(name, accounts, balance) {
+    this.name = name;
+    this.accounts = accounts;
+    this.balance = balance;
+  }
+
+  openAccount() {
+    this.accounts.push(new BankAccount("123456789", 1000));
+  }
+
+  closeAccount() {
+    this.accounts.pop();
+  }
+
+  getBalance() {
+    return this.balance;
+  }
+
+  totalBalance() {
+    let sum = 0;
+    for (let i = 0; i < this.accounts.length; i++) {
+      sum += this.accounts[i].getBalance();
+    }
+    return sum;
+  }
+}
