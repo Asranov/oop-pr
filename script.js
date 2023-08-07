f82960f080d161ffa9e549ec6076c30e8cf4424c
Asranov <asranov0003@gmail.com>
2023-08-07 10:43:09 +0500

    extends super fun

--- script.js
+++ script.js
@@ -241,3 +241,30 @@ class MyNumbers {
 
 const myNumbers = new MyNumbers([1, 2, 3, 4, 5]);
 myNumbers.displayNumber();
+
+//
+class Book {
+  constructor(title, author, publicationYear) {
+    this.title = title;
+    this.author = author;
+    this.publicationYear = publicationYear;
+  }
+
+  displayDetails() {
+    console.log(`Title: ${this.title}`);
+    console.log(`Author: ${this.author}`);
+    console.log(`Publication Year: ${this.publicationYear}`);
+  }
+}
+
+class Ebook extends Book {
+  constructor(title, author, publicationYear, price) {
+    super(title, author, publicationYear);
+    this.price = price;
+  }
+
+  displayDetails() {
+    super.displayDetails();
+    console.log(`Price: ${this.price}`);
+  }
+}
