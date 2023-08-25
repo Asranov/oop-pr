bb89e941b82f72dfaf4d0ad6e23a49ecca2d7a82
Asranov <asranov0003@gmail.com>
2023-08-25 11:22:06 +0500

    public, private, protected

--- mastery.js
+++ mastery.js
@@ -16,3 +16,75 @@ function squareOrSum(a, b, c) {
 
 console.log("Square function", squareOrSum(2, 3));
 console.log("Sum function", squareOrSum(2, 3, 4));
+
+//Public, Private, Protected
+class User {
+  name = "Bill"; // Public
+  #age = 20; // Private
+  _isMarried = false; // Protected
+
+  constructor() {}
+
+  introduce() {
+    return `My name is ${this.name}, I am ${this.#age} years old, and I am ${
+      this._isMarried ? "married" : "not married"
+    }.`;
+  }
+}
+
+const user = new User();
+
+console.log("Name:", user.name); //Bill
+console.log("Age:", user.age); //undefined
+console.log("IsMaried:", user._isMarried); //false
+
+class Person {
+  #name; //Private
+
+  constructor(name) {
+    this.#name = name;
+  }
+
+  sayHello() {
+    return `Hello, my name is ${this.#name}`;
+  }
+}
+
+const person = new Person("Alex");
+console.log(person.sayHello());
+
+class Employee extends Person {
+  _position; //Protected
+
+  constructor(name, position) {
+    super(name);
+    this._position = position;
+  }
+
+  introduce() {
+    return `${super.sayHello()}. I work as a ${this._position}.`;
+  }
+}
+
+const employee = new Employee("Ali", "Software engineer");
+console.log(employee.sayHello());
+console.log(employee.introduce());
+console.log(employee._position);
+console.log(employee.name); // undefined
+
+class Manager extends Employee {
+  constructor(name, position, department) {
+    super(name, position);
+    this.department = department;
+  }
+
+  getDepartmentInfo() {
+    return `${super.sayHello()}, ${this._position} in the ${
+      this.department
+    } department`;
+  }
+}
+
+const manager = new Manager("Manager", "Manager position", "Business Center");
+console.log(manager._position);
+console.log(manager.getDepartmentInfo());
