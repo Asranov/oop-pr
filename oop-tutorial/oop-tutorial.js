3f5b5f65050bccb8a7cf4f67f94ac804899faa7a
Asranov <asranov0003@gmail.com>
2023-08-31 10:19:10 +0500

    extends class

--- oop-tutorial/oop-tutorial.js
+++ oop-tutorial/oop-tutorial.js
@@ -33,3 +33,22 @@ class Robot {
   transform = () => console.log("Optimus prime!");
   sayPhrase = () => console.log(this.phrase);
 }
+
+class Enemy {
+  constructor(power) {
+    this.power = power;
+  }
+
+  attack = () => console.log(`I'm attacking with a power of ${this.power}!`);
+}
+
+class Alien extends Enemy {
+  constructor(name, phrase, power) {
+    super(power);
+    this.name = name;
+    this.phrase = phrase;
+    this.species = "alien";
+  }
+  fly = () => console.log("Zzzzzziiiiiinnnnnggggg!!");
+  sayPhrase = () => console.log(this.phrase);
+}
